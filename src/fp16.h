dd954be31b87f79e744adb306d20179e761b684e
Richard King <47585928+kbitc@users.noreply.github.com>
2019-05-29 19:52:14 +0800

    Create fp16.h

---
+++ src/fp16.h
@@ -0,0 +1,14 @@
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef unsigned short half;
+
+float htof(half h);
+half ftoh(float f);
